26eaaeee182f669e517135093b6f0fdb143ad07a
mellkior <38818928+mellkior@users.noreply.github.com>
2025-11-16 18:38:49 +0000

    added macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "learning macroquad".to_owned(),
+        window_height: 600,
+        window_width: 800,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        // clear_background(GREEN);
+        next_frame().await
+    }
 }
