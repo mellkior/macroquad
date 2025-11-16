849263cc7388c19fc5b2a463751b1a8fefc15c72
mellkior <38818928+mellkior@users.noreply.github.com>
2025-11-16 20:19:21 +0000

    added random squares

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,32 @@
-use macroquad::prelude::*;
-
-fn window_conf() -> Conf {
-    Conf {
-        window_title: "learning macroquad".to_owned(),
-        window_height: 1200,
-        window_width: 1800,
-        fullscreen: false,
-        ..Default::default()
-    }
-}
+use macroquad::{prelude::*, rand::ChooseRandom};
 
 struct Shape {
     size: f32,
     speed: f32,
     x: f32,
     y: f32,
+    color: Color,
 }
 
-#[macroquad::main(window_conf)]
+#[macroquad::main("learning macroquad")]
 async fn main() {
     const MOVEMENT_SPEED: f32 = 200.0;
-    rand::srand(miniquad::date::now() as u64);
+    let colors: Vec<Color> = vec![RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE];
 
-    let mut squares: Vec<Shape> = vec![];
+    rand::srand(miniquad::date::now() as u64);
+    let mut squares = vec![];
     let mut circle = Shape {
         size: 32.0,
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
+        color: YELLOW,
     };
 
     loop {
-        let delta_time = get_frame_time();
         clear_background(DARKPURPLE);
 
+        let delta_time = get_frame_time();
         if is_key_down(KeyCode::Right) {
             circle.x += circle.speed * delta_time;
         }
@@ -47,10 +40,41 @@ async fn main() {
             circle.y -= circle.speed * delta_time;
         }
 
+        // Clamp X and Y to be within the screen
         circle.x = clamp(circle.x, 0.0, screen_width());
         circle.y = clamp(circle.y, 0.0, screen_height());
 
-        draw_circle(circle.x, circle.y, circle.size, YELLOW);
+        // Generate a new square
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
+                y: -size,
+                color: *colors.choose().unwrap(),
+            });
+        }
+
+        // Move squares
+        for square in &mut squares {
+            square.y += square.speed * delta_time;
+        }
+
+        // Remove squares below bottom of screen
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        // Draw everything
+        draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
+        for square in &squares {
+            draw_rectangle(
+                square.x - square.size / 2.0,
+                square.y - square.size / 2.0,
+                square.size,
+                square.size,
+                square.color,
+            );
+        }
 
         next_frame().await
     }
